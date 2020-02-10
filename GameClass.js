081856553df84e0bcc650f33974c4bc7ead9f535
stebssbets <stebssbets@yahoo.com>
2020-02-10 21:45:46 +0700

    trying to wrap my head around the next step of refactoring. created GameClass and GameClassManager and gameClassTests.js

--- GameClass.js
+++ GameClass.js
@@ -1,127 +1,150 @@
 function GameClass()
 {
 
-  this.Player =
+  this.gameFrameRate = undefined;//number
+
+  this.updateEverythingInTheGame = function(){};
+  this.drawEverythingInTheGame = function(){};
+
+  this.advanceGameFrame = function()
   {
-    coordinates: {x:undefined,y:undefined},
-    xySpeeds: {x:undefined,y:undefined},
-    dimensions: {width:undefined,height:undefined},
-    placeholderColor: undefined,
-    image: undefined,
-
-    update: function()
-    {
-
-    },
-
-    drawPlaceHolderImage: function()
-    {
-      gameCanvasContext.fillStyle = this.placeholderColor;
-      gameCanvasContext.fillRect(this.coordinates.x,this.coordinates.y, this.dimensions.width,this.dimensions.height);
-    },
-
-    drawImage: function()
-    {
-      //drawImage(image,startingPointOfOriginalImageX (optional),startingPointOfOriginalImageY (optional),
-      //                cropWidthOfOriginalImage (optional),cropWidthOfOriginalImage (optional),
-      //                startingXofDrawingOnCanvas,startingYofDrawingOnCanvas,
-      //                widthOfDrawingOnCanvas,height);
-      gameCanvasContext.drawImage(this.image, this.coordinates.x,this.coordinates.y,
-                                              this.this.dimensions.width,this.dimensions.height);
-    },
-
-    draw: function()
-    {
-      if (this.image === undefined)
-      {
-        this.drawPlaceHolderImage();
-      } else
-      {
-        this.drawImage();
-      }
-
-      // for(let snakeTailIndex = 0; snakeTailIndex < snakeTail.length; snakeTailIndex++)
-      // {
-      //   gameCanvasContext.fillRect(snakeTail[snakeTailIndex].x,snakeTail[snakeTailIndex].y,
-      //   snakeDimension - 2,snakeDimension - 2);
-      // }
-    }
-  }
-
-  this.backgroundImage = undefined;
-  this.placeholderBackgroundColor = undefined;
-
-  this.drawPlaceHolderBackgroundImage = function()
-  {
-    gameCanvasContext.fillStyle = this.placeholderBackgroundColor;
-    gameCanvasContext.fillRect(0,0, gameCanvas.width,gameCanvas.height);
-  }
-
-  this.drawBackgroundImage = function()
-  {
-    //drawImage(image,startingPointOfOriginalImageX (optional),startingPointOfOriginalImageY (optional),
-    //                cropWidthOfOriginalImage (optional),cropWidthOfOriginalImage (optional),
-    //                startingXofDrawingOnCanvas,startingYofDrawingOnCanvas,
-    //                widthOfDrawingOnCanvas,height);
-    gameCanvasContext.drawImage(this.backgroundImage, this.coordinates.x,this.coordinates.y,
-                                                      this.this.dimensions.width,this.dimensions.height);
-  }
-
-  this.drawBackground = function()
-  {
-    if (this.background === undefined)
-    {
-      this.drawPlaceHolderBackgroundImage();
-    } else
-    {
-      this.drawBackgroundImage();
-    }
-  }
-
-  this.frameRate = undefined;
-
-  this.backButtonSpecifics =
-  {
-    backButtonRectangleColor: undefined,
-    backButtonTextColor: undefined
-  }
-
-  this.answersSpeed = undefined;
-
-  this.canvasDimensions = {width:undefined,height:undefined};
-
-  this.init = function()
-  {
-
-  };
-  this.update = function()
-  {
-
-  };
-	this.movePlayer = function()
-  {
-
-  };
-	this.handleSpritesOffScreen = function()
-  {
-
-	};
-	this.moveAnswersIfAppropriate = function()
-  {
-
-  };
-	this.handleCollisionsWithAnswers = function()
-  {
-
-  };
-	this.draw = function()
-  {
-    gameCanvasContext.fillStyle = undefined;
-    gameCanvasContext.fillRect(0,0, gameCanvas.width,gameCanvas.height);
+    // this.updateEverythingInTheGame();
+    // this.drawEverythingInTheGame();
   };
 
-  this.input = function()
-  {
+}
 
+let testGame1 = new GameClass();
+
+testGame1.background =
+{
+  color:'black',
+  startingX:0,
+  startingY:0,
+  width:640,
+  height:700,
+  draw: function()
+  {
+    gameCanvasContext.fillStyle = this.color;
+    gameCanvasContext.fillRect(this.startingX,this.startingY, this.width,this.height);
   }
+}
+
+testGame1.sprite =
+{
+  color: 'blue',
+  x:0,
+  y:0,
+  width:50,
+  height:50,
+
+  update: function(){this.x = this.x + 5, this.y = this.y + 5},
+
+  draw: function()
+  {
+    gameCanvasContext.fillStyle = this.color;
+    gameCanvasContext.fillRect(this.x,this.y, this.width,this.height);
+  }
+}
+
+testGame1.updateEverythingInTheGame = function()
+{
+  testGame1.sprite.update();
+}
+
+testGame1.drawEverythingInTheGame = function()
+{
+  testGame1.background.draw();
+  testGame1.sprite.draw();
+}
+
+testGame1.advanceGameFrame = function()
+{
+  testGame1.updateEverythingInTheGame();
+  testGame1.drawEverythingInTheGame();
+}
+
+testGame1.gameFrameRate = 1000/30;
+
+
+
+let testGame2 = new GameClass();
+
+testGame2.gameFrameRate = 1000/15;
+testGame2.background =
+{
+  color:'blue',
+  startingX:0,
+  startingY:0,
+  width:640,
+  height:700,
+  draw: function()
+  {
+    gameCanvasContext.fillStyle = this.color;
+    gameCanvasContext.fillRect(this.startingX,this.startingY, this.width,this.height);
+  }
+}
+
+testGame2.sprite =
+{
+  color: 'orange',
+  x:500,
+  y:400,
+  width:50,
+  height:50,
+
+  update: function(){this.x = this.x - 5, this.y = this.y - 5},
+
+  draw: function()
+  {
+    gameCanvasContext.fillStyle = this.color;
+    gameCanvasContext.fillRect(this.x,this.y, this.width,this.height);
+  }
+}
+
+testGame2.updateEverythingInTheGame = function()
+{
+  testGame2.sprite.update();
+}
+
+testGame2.drawEverythingInTheGame = function()
+{
+  testGame2.background.draw();
+  testGame2.sprite.draw();
+}
+
+testGame2.advanceGameFrame = function()
+{
+  testGame2.updateEverythingInTheGame();
+  testGame2.drawEverythingInTheGame();
+}
+
+function GameClassManager()
+{
+  this.currentGame = undefined;
+  this.loadCurrentGame = function(gameToLoad)
+  {
+    this.currentGame = gameToLoad;
+  }
+}
+
+let gameClassManager = new GameClassManager();
+
+var gameCanvas, gameCanvasContext, statsCanvas, statsCanvasContext, runnerGame;
+window.onload = function()
+{
+  gameCanvas = document.getElementById("gameCanvas");
+  gameCanvasContext = gameCanvas.getContext('2d');
+
+  let randomNumber = Math.random();
+  if (randomNumber < 0.5)
+  {
+    console.log('should load game 1');
+    gameClassManager.loadCurrentGame(testGame1);
+  } else {
+    console.log('should load game 2');
+    gameClassManager.loadCurrentGame(testGame2);
+  }
+
+  setInterval(gameClassManager.currentGame.advanceGameFrame, gameClassManager.currentGame.gameFrameRate);
 }
