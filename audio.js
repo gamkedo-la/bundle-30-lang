87850f6fb722cf8395085d74aeefa1760dae7b53
stebssbets <stebssbets@yahoo.com>
2020-02-24 13:33:19 +0700

    WIP: Making an audioManager

--- audio.js
+++ audio.js
@@ -1,103 +1,142 @@
-//prompts and answers section
-var womanAudio = document.createElement("AUDIO");
-var womenAudio = document.createElement("AUDIO");
-var menAudio = document.createElement("AUDIO");
-var manAudio = document.createElement("AUDIO");
-var heAudio = document.createElement("AUDIO");
-var sheAudio = document.createElement("AUDIO");
-
-//UI section
-var arrayOfUIButtonSounds = [];
-var arrayOfGeneralPositiveFeedbackSounds = [];
-var arrayOfGeneralNegativeFeedbackSounds = [];
-
-var uiButtonSound1 = document.createElement("AUDIO");
-var uiButtonSound2 = document.createElement("AUDIO");
-var uiButtonSound3 = document.createElement("AUDIO");
-var uiButtonSound4 = document.createElement("AUDIO");
-
-//gameplay feedback
-var generalPositiveFeedbackSound1 = document.createElement("AUDIO");
-var generalPositiveFeedbackSound2 = document.createElement("AUDIO");
-var generalPositiveFeedbackSound3 = document.createElement("AUDIO");
-var generalPositiveFeedbackSound4 = document.createElement("AUDIO");
-
-var generalNegativeFeedbackSound1 = document.createElement("AUDIO");
-var generalNegativeFeedbackSound2 = document.createElement("AUDIO");
-var generalNegativeFeedbackSound3 = document.createElement("AUDIO");
-var generalNegativeFeedbackSound4 = document.createElement("AUDIO");
-
-
-//music
-var currentBackgroundMusic = undefined;
-
-var transitionToLevelMusic1 = document.createElement("AUDIO");
-var titleScreenMusic = document.createElement("AUDIO");
-
-var runnerBackgroundMusic = document.createElement("AUDIO");
-var pinataBackgroundMusic = document.createElement("AUDIO");
-
-
-
-function setSourcesForAudioObjects()//for after loading screen
+function AudioManager()
 {
 
-  //prompts and answers
-  womanAudio.src = 'audio/PromptsAndAnswers/woman.mp3';
-  masterAudioBus.push(womanAudio);
-  womenAudio.src = 'audio/PromptsAndAnswers/women.mp3';
-  masterAudioBus.push(womenAudio);
-  menAudio.src = 'audio/PromptsAndAnswers/men.mp3';
-  masterAudioBus.push(menAudio);
-  manAudio.src = 'audio/PromptsAndAnswers/man.mp3';
-  masterAudioBus.push(manAudio);
-  heAudio.src = 'audio/PromptsAndAnswers/he.mp3';
-  masterAudioBus.push(heAudio);
-  sheAudio.src = 'audio/PromptsAndAnswers/she.mp3';
-  masterAudioBus.push(sheAudio);
+  this.createHTMLDocumentAudioTags = function()
+  {
+    //prompts and answers section
+    this.womanAudio = document.createElement("AUDIO");
+    this.womenAudio = document.createElement("AUDIO");
+    this.menAudio = document.createElement("AUDIO");
+    this.manAudio = document.createElement("AUDIO");
+    this.heAudio = document.createElement("AUDIO");
+    this.sheAudio = document.createElement("AUDIO");
+
+    //UI section
+    this.uiButtonSound1 = document.createElement("AUDIO");
+    this.uiButtonSound2 = document.createElement("AUDIO");
+    this.uiButtonSound3 = document.createElement("AUDIO");
+    this.uiButtonSound4 = document.createElement("AUDIO");
+
+    //gameplay feedback
+    this.generalPositiveFeedbackSound1 = document.createElement("AUDIO");
+    this.generalPositiveFeedbackSound2 = document.createElement("AUDIO");
+    this.generalPositiveFeedbackSound3 = document.createElement("AUDIO");
+    this.generalPositiveFeedbackSound4 = document.createElement("AUDIO");
+
+    this.generalNegativeFeedbackSound1 = document.createElement("AUDIO");
+    this.generalNegativeFeedbackSound2 = document.createElement("AUDIO");
+    this.generalNegativeFeedbackSound3 = document.createElement("AUDIO");
+    this.generalNegativeFeedbackSound4 = document.createElement("AUDIO");
+
+    //background music
+    this.transitionToLevelMusic1 = document.createElement("AUDIO");
+    this.titleScreenMusic = document.createElement("AUDIO");
+    this.runnerBackgroundMusic = document.createElement("AUDIO");
+    this.pinataBackgroundMusic = document.createElement("AUDIO");
+  }
+
+  this.promptsAndAnswersBus = [];
+
+  this.initializePromptsAndAnswersBus = function()
+  {
+    this.promptsAndAnswersBus.push(this.womanAudio);
+    this.promptsAndAnswersBus.push(this.womenAudio);
+    this.promptsAndAnswersBus.push(this.menAudio);
+    this.promptsAndAnswersBus.push(this.manAudio);
+    this.promptsAndAnswersBus.push(this.heAudio);
+    this.promptsAndAnswersBus.push(this.sheAudio);
+  }
 
 
-  //gameplay feedback
-  generalPositiveFeedbackSound1.src = "audio/Positive_01.mp3";
-  masterAudioBus.push(generalPositiveFeedbackSound1);
-  generalPositiveFeedbackSound2.src = "audio/Positive_02.mp3";
-  masterAudioBus.push(generalPositiveFeedbackSound2);
-  generalPositiveFeedbackSound3.src = "audio/Positive_03.mp3";
-  masterAudioBus.push(generalPositiveFeedbackSound3);
-  generalPositiveFeedbackSound4.src = "audio/Positive_04.mp3";
-  masterAudioBus.push(generalPositiveFeedbackSound4);
+  //UI section
+  this.arrayOfUIButtonSounds = [];
+  this.arrayOfGeneralPositiveFeedbackSounds = [];
+  this.arrayOfGeneralNegativeFeedbackSounds = [];
 
-  generalNegativeFeedbackSound1.src = "audio/Negative_01.mp3";
-  masterAudioBus.push(generalNegativeFeedbackSound1);
-  generalNegativeFeedbackSound2.src = "audio/Negative_02.mp3";
-  masterAudioBus.push(generalNegativeFeedbackSound2);
-  generalNegativeFeedbackSound3.src = "audio/Negative_03.mp3";
-  masterAudioBus.push(generalNegativeFeedbackSound3);
-  generalNegativeFeedbackSound4.src = "audio/Negative_04.mp3";
-  masterAudioBus.push(generalNegativeFeedbackSound4);
 
-  //UI
-  uiButtonSound1.src = "audio/UI_01.mp3";
-  masterAudioBus.push(uiButtonSound1);
-  uiButtonSound2.src = "audio/UI_02.mp3";
-  masterAudioBus.push(uiButtonSound2);
-  uiButtonSound3.src = "audio/UI_03.mp3";
-  masterAudioBus.push(uiButtonSound3);
-  uiButtonSound4.src = "audio/UI_04.mp3";
-  masterAudioBus.push(uiButtonSound4);
+  this.SFXBus = [];
 
-  //music
-  transitionToLevelMusic1.src = "audio/levelTransitionSound.mp3";
-  masterAudioBus.push(transitionToLevelMusic1);
-  titleScreenMusic.src = 'audio/backgroundTracks/titleScreenMusic.mp3';
-  masterAudioBus.push(titleScreenMusic);
+  this.initializeSFXBus = function()
+  {
+    //UI
+    this.SFXBus.push(this.uiButtonSound1);
+    this.SFXBus.push(this.uiButtonSound2);
+    this.SFXBus.push(this.uiButtonSound3);
+    this.SFXBus.push(this.uiButtonSound4);
 
-  runnerBackgroundMusic.src = "audio/backgroundTracks/runnerBackground.mp3";
-  masterAudioBus.push(runnerBackgroundMusic);
-  pinataBackgroundMusic.src = "audio/backgroundTracks/pinataBackgroundMusic.mp3";
-  pinataBackgroundMusic.volume = 0.2;
-  masterAudioBus.push(pinataBackgroundMusic);
+    //correct and incorrect answer feedback sounds
+    this.SFXBus.push(this.generalPositiveFeedbackSound1);
+    this.SFXBus.push(this.generalPositiveFeedbackSound2);
+    this.SFXBus.push(this.generalPositiveFeedbackSound3);
+    this.SFXBus.push(this.generalPositiveFeedbackSound4);
+
+    this.SFXBus.push(this.generalNegativeFeedbackSound1);
+    this.SFXBus.push(this.generalNegativeFeedbackSound2);
+    this.SFXBus.push(this.generalNegativeFeedbackSound3);
+    this.SFXBus.push(this.generalNegativeFeedbackSound4);
+  }
+
+  this.currentBackgroundMusic = undefined;
+
+  this.backgroundMusicBus = [];
+
+  this.initializeArrayOfBackgroundMusicTracks = function()
+  {
+    this.backgroundMusicBus.push(this.runnerBackgroundMusic);
+    this.backgroundMusicBus.push(this.pinataBackgroundMusic);
+    this.backgroundMusicBus.push(this.transitionToLevelMusic1);
+    this.backgroundMusicBus.push(this.titleScreenMusic);
+  }
+
+  this.setSourcesForAudioObjects = function()
+  {
+      //prompts and answers
+      this.womanAudio.src = 'audio/PromptsAndAnswers/woman.mp3';
+      this.womenAudio.src = 'audio/PromptsAndAnswers/women.mp3';
+      this.menAudio.src = 'audio/PromptsAndAnswers/men.mp3';
+      this.manAudio.src = 'audio/PromptsAndAnswers/man.mp3';
+      this.heAudio.src = 'audio/PromptsAndAnswers/he.mp3';
+      this.sheAudio.src = 'audio/PromptsAndAnswers/she.mp3';
+
+      //gameplay feedback
+      this.generalPositiveFeedbackSound1.src = "audio/Positive_01.mp3";
+      this.generalPositiveFeedbackSound2.src = "audio/Positive_02.mp3";
+      this.generalPositiveFeedbackSound3.src = "audio/Positive_03.mp3";
+      this.generalPositiveFeedbackSound4.src = "audio/Positive_04.mp3";
+
+      this.generalNegativeFeedbackSound1.src = "audio/Negative_01.mp3";
+      this.generalNegativeFeedbackSound2.src = "audio/Negative_02.mp3";
+      this.generalNegativeFeedbackSound3.src = "audio/Negative_03.mp3";
+      this.generalNegativeFeedbackSound4.src = "audio/Negative_04.mp3";
+
+      //UI
+      this.uiButtonSound1.src = "audio/UI_01.mp3";
+      this.uiButtonSound2.src = "audio/UI_02.mp3";
+      this.uiButtonSound3.src = "audio/UI_03.mp3";
+      this.uiButtonSound4.src = "audio/UI_04.mp3";
+
+      //music
+      this.transitionToLevelMusic1.src = "audio/levelTransitionSound.mp3";
+      this.titleScreenMusic.src = 'audio/backgroundTracks/titleScreenMusic.mp3';
+      this.runnerBackgroundMusic.src = "audio/backgroundTracks/runnerBackground.mp3";
+      this.pinataBackgroundMusic.src = "audio/backgroundTracks/pinataBackgroundMusic.mp3";
+      this.pinataBackgroundMusic.volume = 0.2;
+  }
+
+  this.masterBus = [];
+
+  this.initializeMasterBus = function()
+  {
+    this.masterBus.push(this.promptsAndAnswersBus);
+    this.masterBus.push(this.SFXBus);
+    this.masterBus.push(this.backgroundMusicBus);
+  }
+
+  this.arrayOfTransitionMusic = [];
+  this.arrayOfTransitionMusic.push(this.transitionToLevelMusic1);
 }
+
+let audioManager = new AudioManager();
 
 // transitionToLevelMusic1.onended = "correctLetterAudioTag.play()";
 // transitionToLevelMusic1.addEventListener("ended", playCorrectLetterAudioTag());
