eb14c16b4ca406894f2c180a83d5d2298f00c71c
stebssbets <stebssbets@yahoo.com>
2020-03-12 12:42:17 +0700

    more obsolete code deletion. moved common functions to commonFunction.js

--- javascript/promptsAndAnswers/PromptAndAnswerClass.js
+++ javascript/promptsAndAnswers/PromptAndAnswerClass.js
@@ -62,15 +62,3 @@ function initializePromptAndAnswerObjects()
   heVersusShePairGrouping.arrayOfObjects.push(shePromptAndAnswer);
   promptsAndAnswersManager.arrayOfLogicalPromptAnswerGroupings.push(heVersusShePairGrouping);
 }
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-}
-
-function getRandomIntWithExclusionaryRange(min,max, excludedMin,excludedMax) {
-    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    return (randomNumber >= excludedMin && randomNumber <= excludedMax) ?
-    getRandomIntWithExclusionaryRange(min,max, excludedMin,excludedMax) : randomNumber;
-}
