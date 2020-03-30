1bda97ac347dad5d4b14030492a0eb7f2c221107
Michael Fewkes <mdfewkes@gmail.com>
2020-03-30 13:26:06 -0600

    Implemented Maze music

--- javascript/specificGames/maze/MazeGameClass.js
+++ javascript/specificGames/maze/MazeGameClass.js
@@ -39,6 +39,8 @@ function MazeGameClass(){
         }
     ];
 
+    this.backgroundMusic = new MusicTrack('audio/backgroundTracks/mazeRaceMusic.mp3', 11.2);
+
     this.superInitialize = function () {
         this.maze = new MazeClass();
         this.maze.initializeArrayOfCells();
