a857f75691622da334ebb449cd690004305acf4a
Michael Fewkes <mdfewkes@gmail.com>
2020-03-30 21:11:24 -0600

    Slight timing adjustments on title track

--- javascript/audio.js
+++ javascript/audio.js
@@ -229,7 +229,7 @@ genAudio.playTransitionMusic = function() {
 		gameCanvasContext.globalAlpha = 1;
 	}
 }
-genAudio.titleMusic = new MusicTrack('audio/backgroundTracks/titleScreenMusic.mp3', 6.15);
+genAudio.titleMusic = new MusicTrack('audio/backgroundTracks/titleScreenMusic.mp3', 6.1);
 genAudio.playTitleMusic = function() {
 	musicManager.addTrack(genAudio.titleMusic);
 	musicManager.moveToLastTrack();
