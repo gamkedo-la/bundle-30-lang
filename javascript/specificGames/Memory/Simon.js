93a16c93b18ae3ddb59fd26b6dbfe1fadf66a874
stebssbets <stebssbets@yahoo.com>
2020-06-06 18:51:07 +0700

    debugged random range inclusive problem. tweaked letter sizes and positioning on the simon

--- javascript/specificGames/Memory/Simon.js
+++ javascript/specificGames/Memory/Simon.js
@@ -13,22 +13,26 @@ function Simon()
   this.rightPhonic = undefined;
 
   this.leftPhonicX = this.x + this.width*0.15;
-  this.leftPhonicY = this.y + this.height/2 - 50;
+  this.leftPhonicY = this.y + this.height/2 + 10;
   this.rightPhonicX = this.x + this.width - this.width*0.3;
-  this.rightPhonicY = this.y + this.height/2 - 50;
+  this.rightPhonicY = this.y + this.height/2 + 10;
 
   this.pickARandomImage = getRandomIntInclusive(0,this.arrayOfImages.length - 1);
 
   this.grabLeftAndRightPhonics = function()
   {
-    let randomPhonicIndex1 = getRandomIntInclusive(0,gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics.length);
-    let randomPhonicIndex2 = getRandomIntInclusive(0,gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics.length);
+    let randomPhonicIndex1 = getRandomIntInclusive(0,gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics.length - 1);
+    let randomPhonicIndex2 = getRandomIntInclusive(0,gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics.length - 1);
     while (randomPhonicIndex1 === randomPhonicIndex2)
     {
-      randomPhonicIndex2 = getRandomIntInclusive(0,gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics.length);
+      randomPhonicIndex2 = getRandomIntInclusive(0,gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics.length - 1);
     }
     this.leftPhonic = gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics[randomPhonicIndex1];
+    console.log('randomPhonicIndex1: ' + randomPhonicIndex1);
+    console.log('this.leftPhonic: ' + this.leftPhonic);
     this.rightPhonic = gameClassManager.currentGame.phonicClassManager.temporaryArrayOfPhonics[randomPhonicIndex2];
+    console.log('randomPhonicIndex1: ' + randomPhonicIndex2);
+    console.log('this.rightPhonic: ' + this.rightPhonic);
   }
 
   this.draw = function()
