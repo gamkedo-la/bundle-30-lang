ba41bc80fb2d6e0b61d3e36bc172ef5d02632fbc
stebssbets <stebssbets@yahoo.com>
2020-06-12 19:12:14 +0700

    did what I could quickly do with fishing game implementation of drawFromSheet()

--- javascript/specificGames/fishing/fishingBackground.js
+++ javascript/specificGames/fishing/fishingBackground.js
@@ -2,11 +2,13 @@ const WATER_HEIGHT = 210;
 
 function FishingBackground()
 {
-  this.waterImage = fishingGameWaterBackgroundImage;
-  this.skyImage = skyBackground;
+  this.waterImage = 'images\\Backgrounds\\underwater2.png';
+  this.skyImage = 'images\\Backgrounds\\runnerSunAndSky.png';
   this.draw = function()
   {
-    gameCanvasContext.drawImage(this.skyImage, 0,0, gameCanvas.width,gameCanvas.height*0.3);
-    gameCanvasContext.drawImage(this.waterImage, 0,gameCanvas.height*0.3, gameCanvas.width,gameCanvas.height*0.7);
+    drawFromSheet(this.skyImage, 0,0, gameCanvas.width,gameCanvas.height*0.3);
+    drawFromSheet(this.waterImage, 0,gameCanvas.height*0.3, gameCanvas.width,gameCanvas.height*0.7);
+    //gameCanvasContext.drawImage(this.skyImage, 0,0, gameCanvas.width,gameCanvas.height*0.3);
+    //gameCanvasContext.drawImage(this.waterImage, 0,gameCanvas.height*0.3, gameCanvas.width,gameCanvas.height*0.7);
   }
 }
